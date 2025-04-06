a3a4cfaf0dc7628cc6cdc00e7f432478372a6e4c
NorayrZalyan <norayr.zalyan2009@gmail.com>
2025-04-06 13:42:44 +0400

    bug fixes

--- app/src/main/java/my/app/chordmate/LoginActivity.java
+++ app/src/main/java/my/app/chordmate/LoginActivity.java
@@ -56,6 +56,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
             }
         });
     }
